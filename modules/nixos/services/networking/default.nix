52f102d037cc8c6224dc801f8fc42481ba5bca10
nttis <42465069+nttis@users.noreply.github.com>
2024-12-24 00:34:57 +0700

    refactor: replaced wpa_supplicant with iwd

--- modules/nixos/services/networking/default.nix
+++ modules/nixos/services/networking/default.nix
@@ -9,6 +9,11 @@ lib.${namespace}.mkModule ./. config {
 } {
   networking.networkmanager = {
     enable = true;
+    wifi.backend = "iwd";
+  };
+
+  networking.wireless.iwd = {
+    enable = true;
   };
 
   programs.nm-applet.enable = true;
