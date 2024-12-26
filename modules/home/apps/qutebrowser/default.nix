3d5fc30dfdcad126ed7b2db7f8a1abc5434eba6c
nttis <42465069+nttis@users.noreply.github.com>
2024-12-26 22:00:36 +0700

    feat: added blocklists to qutebrowser

--- modules/home/apps/qutebrowser/default.nix
+++ modules/home/apps/qutebrowser/default.nix
@@ -9,5 +9,22 @@ lib.${namespace}.mkModule ./. config {
 } {
   programs.qutebrowser = {
     enable = true;
+    settings = {
+      content.blocking = {
+        adblock.lists = [
+          "https://easylist.to/easylist/easylist.txt"
+          "https://easylist.to/easylist/easyprivacy.txt"
+          "https://secure.fanboy.co.nz/fanboy-annoyance.txt"
+          "https://github.com/uBlockOrigin/uAssets/raw/master/filters/privacy.txt"
+        ];
+
+        hosts.lists = [
+          "https://pgl.yoyo.org/as/serverlist.php?hostformat=&showintro=0&mimetype=plaintext"
+          "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
+        ];
+
+        method = "both";
+      };
+    };
   };
 }
