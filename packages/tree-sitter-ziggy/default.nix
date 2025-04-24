cfa09e40ddf599f4db54505ce1550dc26c1d0e8f
nttis <slush-gulf-rural@duck.com>
2025-04-24 15:42:15 +0700

    pkgs/tree-sitter-ziggy: init

---
+++ packages/tree-sitter-ziggy/default.nix
@@ -0,0 +1,25 @@
+{pkgs, ...}:
+pkgs.stdenv.mkDerivation rec {
+  pname = "tree-sitter-ziggy";
+  version = "fe3bf9389e7ff213cf3548caaf9c6f3d4bb38647";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "kristoff-it";
+    repo = "ziggy";
+    rev = version;
+    hash = "sha256-w2WO2N3+XJWhWnt9swOux2ynKxmePbB4VojXM8K5GAo=";
+  };
+
+  buildPhase = ''
+    runHook preBuild
+    $CC -shared -o ziggy.so tree-sitter-ziggy/src/*.c
+    runHook postBuild
+  '';
+
+  installPhase = ''
+    mkdir -p $out
+
+    cp ziggy.so $out/parser
+    cp -r tree-sitter-ziggy/queries $out/
+  '';
+}
