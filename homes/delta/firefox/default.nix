955a3aa46bbfbcd52351ce1807244a8a3fe93212
nttis <slush-gulf-rural@duck.com>
2025-05-19 23:33:10 +0700

    librewolf: switch to firefox

---
+++ homes/delta/firefox/default.nix
@@ -0,0 +1,147 @@
+{
+  lib,
+  pkgs,
+  config,
+  osConfig,
+  ...
+}: {
+  programs.firefox = {
+    enable = true;
+
+    profiles.delta = {
+      name = "delta";
+      isDefault = true;
+
+      search = {
+        default = "ddg";
+        privateDefault = "ddg";
+        force = true;
+
+        engines = {
+          "google".metaData.hidden = true;
+          "bing".metaData.hidden = true;
+          "wikipedia".metaData.hidden = true;
+        };
+      };
+
+      settings = {
+        "extensions.autoDisableScopes" = 0;
+        "extensions.pocket.enabled" = false;
+
+        "browser.aboutConfig.showWarning" = false;
+        "browser.translations.enable" = false;
+        "browser.search.separatePrivateDefault" = false;
+        "browser.toolbars.bookmarks.visibility" = "never";
+        "browser.search.suggest.enabled" = true;
+        "browser.startup.page" = 3;
+        "browser.newtabpage.enabled" = false;
+
+        "dom.security.https_only_mode" = true;
+        "dom.security.https_only_mode_ever_enabled" = true;
+
+        "doh-rollout.disable-heuristics" = true;
+        "network.trr.mode" = 5;
+        "signon.rememberSignons" = false;
+
+        "app.shield.optoutstudies.enabled" = false;
+        "browser.discovery.enabled" = false;
+        "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+        "browser.newtabpage.activity-stream.telemetry" = false;
+        "browser.ping-centre.telemetry" = false;
+        "datareporting.healthreport.service.enabled" = false;
+        "datareporting.healthreport.uploadEnabled" = false;
+        "datareporting.policy.dataSubmissionEnabled" = false;
+        "datareporting.sessions.current.clean" = true;
+        "devtools.onboarding.telemetry.logged" = false;
+        "toolkit.telemetry.archive.enabled" = false;
+        "toolkit.telemetry.bhrPing.enabled" = false;
+        "toolkit.telemetry.enabled" = false;
+        "toolkit.telemetry.firstShutdownPing.enabled" = false;
+        "toolkit.telemetry.hybridContent.enabled" = false;
+        "toolkit.telemetry.newProfilePing.enabled" = false;
+        "toolkit.telemetry.prompted" = 2;
+        "toolkit.telemetry.rejected" = true;
+        "toolkit.telemetry.reportingpolicy.firstRun" = false;
+        "toolkit.telemetry.server" = "";
+        "toolkit.telemetry.shutdownPingSender.enabled" = false;
+        "toolkit.telemetry.unified" = false;
+        "toolkit.telemetry.unifiedIsOptIn" = false;
+        "toolkit.telemetry.updatePing.enabled" = false;
+
+        "identity.fxaccounts.enabled" = false;
+      };
+
+      extensions = {
+        force = true;
+
+        packages = with pkgs.firefox-addons; [
+          ublock-origin
+          sponsorblock
+          bitwarden
+          duckduckgo-privacy-essentials # Just to enable the use of Duck emails smh
+          tampermonkey
+        ];
+
+        settings = {
+          "firefox@tampermonkey.net" = {
+            force = true;
+            settings = builtins.fromJSON (lib.readFile ./tampermonkey.json);
+          };
+
+          "sponsorBlocker@ajay.app" = {
+            force = true;
+            settings = {
+              alreadyInstalled = true;
+            };
+          };
+
+          "uBlock0@raymondhill.net" = {
+            force = true;
+            settings = {
+              selectedFilterLists = [
+                "ublock-filters"
+                "ublock-badware"
+                "ublock-privacy"
+                "ublock-abuse"
+                "ublock-unbreak"
+                "ublock-annoyances"
+
+                "easylist"
+                "easyprivacy"
+                "fanboy-social"
+                "fanboy-thirdparty_social"
+                "fanboy-cookiemonster"
+                "ublock-cookies-easylist"
+                "easylist-chat"
+                "easylist-newsletters"
+                "easylist-notifications"
+                "easylist-annoyances"
+
+                "adguard-social"
+                "adguard-generic"
+                "adguard-mobile"
+                "adguard-cookies"
+                "ublock-cookies-adguard"
+                "adguard-mobile-app-banners"
+                "adguard-other-annoyances"
+                "adguard-popup-overlays"
+                "adguard-widgets"
+
+                "urlhaus-1"
+                "plowe-0"
+              ];
+            };
+          };
+        };
+      };
+    };
+  };
+
+  stylix = lib.mkIf config.stylix.enable {
+    targets.firefox.profileNames = ["delta"];
+  };
+
+  home.persistence."/persist/delta" = lib.mkIf osConfig.impermanence.enable {
+    directories = [".mozilla/firefox"];
+  };
+}
