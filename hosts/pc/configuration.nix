6adf2db9e9e9f851a49b7b0546b6ad2483b5a105
nttis <slush-gulf-rural@duck.com>
2025-06-20 23:48:09 +0700

    pc: change to kernel 6.6

--- hosts/pc/configuration.nix
+++ hosts/pc/configuration.nix
@@ -36,7 +36,7 @@
   nixpkgs.config.nvidia.acceptLicense = true;
 
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_6;
 
   services.xserver = {
     enable = lib.mkForce true;
