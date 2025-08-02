f5ee3c3e7f7168f4c237f10b0bdfe8e0894433a9
nttis <slush-gulf-rural@duck.com>
2025-08-03 02:15:04 +0700

    power: use power-profiles-daemon

--- modules/nixos/power/default.nix
+++ modules/nixos/power/default.nix
@@ -3,18 +3,22 @@
 {
   imports = [ ];
 
+  services.power-profiles-daemon = {
+    enable = true;
+  };
+
   services.thermald = {
     enable = true;
   };
 
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_SCALING_GOVERNER_ON_AC = "performance";
-    };
-  };
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #     CPU_SCALING_GOVERNER_ON_AC = "performance";
+  #   };
+  # };
 }
