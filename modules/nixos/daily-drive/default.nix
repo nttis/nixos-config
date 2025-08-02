66cd698ccbb6f6559e9bb98d9536065534d903f4
nttis <slush-gulf-rural@duck.com>
2025-08-02 20:24:41 +0700

    daily-drive: replace xdg-open with handlr-regex

--- modules/nixos/daily-drive/default.nix
+++ modules/nixos/daily-drive/default.nix
@@ -61,6 +61,21 @@
     ];
   };
 
+  system.replaceDependencies.replacements = [
+    {
+      oldDependency = pkgs.xdg-utils;
+      newDependency = pkgs.xdg-utils.overrideAttrs (prev: {
+        postFixup = let
+          handlr-script = pkgs.writeShellScript "xdg-open" ''
+            ${pkgs.lib.getExe pkgs.handlr-regex} open "$@"
+          '';
+        in ''
+          cp ${handlr-script} $out/bin/xdg-open
+        '';
+      });
+    }
+  ];
+
   environment = {
     systemPackages = with pkgs; [
       helix
