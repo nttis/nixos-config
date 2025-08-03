d667eb3628c99a5ca3eb3c5568a9d7c5e3bca919
nttis <slush-gulf-rural@duck.com>
2025-08-04 02:09:21 +0700

    mime: update mimetypes
    I have already overridden xdg-open with handlr-regex,
    which should've allowed me to simply use regex
    patterns for the mimetypes. However, that is not
    the case with literally any other piece of code
    that interacts with mime (eg. Gio, xdg-open). This
    renders Flatpak applications unable to use the
    correct mimetypes, since Flatpak uses Gio, which
    does not understand regex mime handlers like
    handlr-regex does.
    
    Now that the mimetypes list is all spelled out
    directly, everything works.
    
    handlr-regex is still here though because it's
    plain better than the horrible bash abomination
    that is xdg-open.

--- modules/home/delta/mimetypes.nix
+++ modules/home/delta/mimetypes.nix
@@ -1,16 +1,62 @@
 { ... }:
+let
+  images = [
+    "png"
+    "apng"
+    "jpeg"
+    "jxl"
+    "svg"
+    "svg+xml"
+    "tiff"
+    "gif"
+    "webp"
+    "avif"
+    "heic"
+    "heif"
+  ];
+
+  audio = [
+    "flac"
+    "aac"
+    "matroska"
+    "mpeg"
+    "ogg"
+    "opus"
+    "vorbis"
+  ];
+
+  schemes = [
+    "http"
+    "https"
+  ];
+
+  mapAssocs =
+    prefix: assocs: list:
+    (builtins.map (x: {
+      name = prefix + x;
+      value = assocs;
+    }) list);
+
+  associations = builtins.listToAttrs (
+    builtins.concatLists [
+      (mapAssocs "audio/" [ "mpv.desktop" ] audio)
+      (mapAssocs "image/" [ "feh.desktop" ] images)
+      (mapAssocs "x-scheme-handler/" [ "org.qutebrowser.qutebrowser.desktop" "firefox.desktop" ] schemes)
+    ]
+  );
+in
 {
   imports = [
     ./feh.nix
+    ./qutebrowser
   ];
 
   programs.mpv.enable = true;
 
+  xdg.mime.enable = true;
   xdg.mimeApps = {
     enable = true;
-    defaultApplications = {
-      "image/*" = [ "feh.desktop" ];
-      "audio/*" = [ "mpv.desktop" ];
-    };
+    defaultApplications = associations;
+    associations.added = associations;
   };
 }
