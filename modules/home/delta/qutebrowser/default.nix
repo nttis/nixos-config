0d7227e67164de3bdf3f3ee91626d84a3bba6c2e
nttis <slush-gulf-rural@duck.com>
2025-08-04 17:19:00 +0700

    qutebrowser: a lot of config

--- modules/home/delta/qutebrowser/default.nix
+++ modules/home/delta/qutebrowser/default.nix
@@ -6,18 +6,43 @@
       content = {
         javascript.enabled = false;
       };
+
+      scrolling = {
+        bar = "overlay";
+      };
+
+      auto_save = {
+        session = true;
+      };
     };
 
     keyBindings = {
       normal = {
-        "<Ctrl-v>" = "spawn mpv {url}";
+        "<Ctrl-v>" = "hint links spawn --detach mpv {hint-url}";
+        "<Escape>" = "mode-enter normal ;; jseval -q document.activeElement.blur()";
+
+        # "go-libre"
+        "gl" = "open https://farside.link/{url}";
+
+        # Swap the j and k bindings
+        "J" = "tab-prev";
+        "K" = "tab-next";
+
+        # Increase scroll amount
+        "j" = "cmd-run-with-count 3 :scroll down";
+        "k" = "cmd-run-with-count 3 :scroll up";
+        "h" = "cmd-run-with-count 3 :scroll left";
+        "l" = "cmd-run-with-count 3 :scroll right";
+
+        "ge" = "scroll-to-perc";
       };
     };
 
     extraConfig = # python
       ''
         config.set("content.javascript.enabled", True, "*://*.nixos.org/*")
-        config.set("content.javascript.enabled", True, "*://*.github.com/*")
+        config.set("content.javascript.enabled", True, "*://*.extranix.com/*")
+        config.set("content.javascript.enabled", True, "*://duckduckgo.com/*")
       '';
   };
 
