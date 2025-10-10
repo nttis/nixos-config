607efdb9516ba929ad3f95ed9bea827c71a7468c
nttis <slush-gulf-rural@duck.com>
2025-10-10 16:59:25 +0700

    scanners: add simple-scan to system packages

--- modules/scanners.nix
+++ modules/scanners.nix
@@ -1,11 +1,17 @@
 { ... }:
 {
-  flake.modules.nixos.scanners = {
-    hardware.sane = {
-      enable = true;
-      dsseries.enable = true;
-      brscan5.enable = true;
-      brscan4.enable = true;
+  flake.modules.nixos.scanners =
+    { pkgs, ... }:
+    {
+      hardware.sane = {
+        enable = true;
+        dsseries.enable = true;
+        brscan5.enable = true;
+        brscan4.enable = true;
+      };
+
+      environment.systemPackages = [
+        pkgs.simple-scan
+      ];
     };
-  };
 }
