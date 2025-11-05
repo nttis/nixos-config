d0b3b12deb622322c57440c6a83586a906b74206
nttis <slush-gulf-rural@duck.com>
2025-11-05 21:39:45 +0700

    ci: init

--- modules/flake.nix
+++ modules/flake.nix
@@ -1,4 +1,8 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 {
   imports = [ inputs.flake-parts.flakeModules.modules ];
+
+  flake.githubActions = inputs.nix-github-actions.lib.mkGithubMatrix {
+    checks = inputs.nixpkgs.lib.getAttrs [ "x86_64-linux" ] self.checks;
+  };
 }
