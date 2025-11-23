05d8a8227cefcd6ac3b7c5af074ee42661f11e8b
nttis <slush-gulf-rural@duck.com>
2025-11-23 16:56:30 +0700

    pc: clean things up

--- modules/hosts/pc/default.nix
+++ modules/hosts/pc/default.nix
@@ -1,40 +1,55 @@
 { self, ... }:
 {
-  flake.modules.nixos."hosts.pc".imports = with self.modules.nixos; [
-    impermanence
-    systemd-boot
-    memtest86
-    networking
-    nix-ld
-    foundation
-    daily-drive
-    nvidia-legacy
-    gnome
+  flake.modules.nixos."hosts.pc" =
+    { pkgs, lib, ... }:
+    {
+      imports = with self.modules.nixos; [
+        impermanence
+        systemd-boot
+        memtest86
+        networking
+        nix-ld
+        foundation
+        daily-drive
+        nvidia-legacy
 
-    self.modules.nixos."users.delta"
-  ];
+        self.modules.nixos."users.delta"
+      ];
 
-  flake.modules.nixos."hosts.pc" = {
-    networking.hostName = "pc";
-    time.timeZone = "Asia/Ho_Chi_Minh";
+      networking.hostName = "pc";
+      time.timeZone = "Asia/Ho_Chi_Minh";
 
-    environment.etc."machine-id".text = "717af9368b07a6ba593fd87f68e4f6f2";
+      services.xserver = {
+        enable = lib.mkForce true;
+        desktopManager.xfce = {
+          enable = true;
+          enableScreensaver = false;
+        };
+      };
 
-    boot.initrd.availableKernelModules = [
-      "xhci_pci"
-      "ahci"
-      "usb_storage"
-      "usbhid"
-      "sd_mod"
-    ];
+      xdg.portal.extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+        pkgs.xdg-desktop-portal-xapp
+      ];
 
-    boot.initrd.kernelModules = [ ];
-    boot.extraModulePackages = [ ];
+      environment.etc."machine-id".text = "717af9368b07a6ba593fd87f68e4f6f2";
 
-    nixpkgs.hostPlatform = "x86_64-linux";
-    hardware.cpu.intel.updateMicrocode = true;
-    hardware.enableRedistributableFirmware = true;
+      boot = {
+        kernelModules = [ "i2c-dev" ];
 
-    system.stateVersion = "24.05";
-  };
+        initrd.availableKernelModules = [
+          "xhci_pci"
+          "ahci"
+          "usb_storage"
+          "usbhid"
+          "sd_mod"
+        ];
+      };
+
+      nixpkgs.hostPlatform = "x86_64-linux";
+      hardware.cpu.intel.updateMicrocode = true;
+      hardware.enableRedistributableFirmware = true;
+
+      system.stateVersion = "24.05";
+    };
 }
