6a7cf0186d9bc4cf0063a8aed18cfbe96d45b13b
nttis <slush-gulf-rural@duck.com>
2025-12-04 15:07:46 +0700

    nix-ld: remove useless libs

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -6,13 +6,8 @@
       programs.nix-ld = {
         enable = true;
         libraries = with pkgs; [
-          stdenv.cc.cc.lib
-          alsa-lib
           pulseaudio
           pipewire
-          wayland
-          sdl3
-          SDL2
         ];
       };
     };
