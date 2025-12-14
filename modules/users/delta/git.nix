646258ecb33931f222eb0058687e3a716f358e97
nttis <nttis@duck.com>
2025-12-14 20:54:05 +0700

    git: remove hardcoded, global user

--- modules/users/delta/git.nix
+++ modules/users/delta/git.nix
@@ -3,21 +3,11 @@
   flake.modules.homeManager."git@delta" = {
     programs.git = {
       enable = true;
-
-      settings = {
-        user.name = "nttis";
-        user.email = "slush-gulf-rural@duck.com";
-      };
     };
 
     programs.jujutsu = {
       enable = true;
       settings = {
-        user = {
-          name = "nttis";
-          email = "slush-gulf-rural@duck.com";
-        };
-
         ui = {
           paginate = "never";
         };
